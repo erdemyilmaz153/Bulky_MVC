46b35455812836fcc401514c3b0556bf3c6893cf
Erdem <Erdem@DESKTOP-0EMBIMK>
2024-07-16 22:19:15 +0300

    Creating categories in models

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace BulkyWeb.Models
+
+// all the columns we create in the table
+// prop + Enter to quickly create property (id, name, etc. in the class)
+// Id is primary key
+
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+    }
+}
