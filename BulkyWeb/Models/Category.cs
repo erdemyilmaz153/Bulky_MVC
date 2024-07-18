545106200bee303c69889caabbcf05eecdb8ab07
Erdem <Erdem@DESKTOP-N2KOQQ1>
2024-07-18 22:24:18 +0300

    Just after Category Crud Operations > Hot Reload vid

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 
 // all the columns we create in the table
 // prop + Enter to quickly create property (id, name, etc. in the class)
@@ -9,11 +11,12 @@
     {
         // [Key] needs to be added here if you want to change 'Id' to anything. It states that this is the primary key.
         // Also, if we write CategoryId, it is also a primary key -- model name + Id
+        
         public int Id { get; set; }
 
         // We can add [Required] here as data annotation.
         public string Name { get; set; }
 
-        public string DisplayOrder { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
